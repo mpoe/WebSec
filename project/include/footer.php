b9d4d805e0826e603ad89c96fa9a257755f1d4f1
Unknown <morten_p_oe@hotmail.com>
2017-11-24 00:00:24 +0100

    Footer created WIP on header

---
+++ project/include/footer.php
@@ -0,0 +1,5 @@
+<div class="footer">
+	<div class="footer_logo">AvatarConnect 2017</div>
+</div>
+
+<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script> <!-- jquery from external source might want to download! -->
--- project/index.php
+++ project/index.php
@@ -4,26 +4,9 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="css/mainstyle.css">
-        <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script> <!-- jquery from external source might want to download! -->
-        <title>Avatar Connect</title>
-        
-    </head>
-    <body>
-        
-        <div class="TopBar">
-            <div class="TopBar_Logo">Avatar Connect</div>
-            <div class="TopBar_Greet">Welcome to AvatarConnect!</div>
-            
-            <form action="api/login.php" method="post" class="TopBar_Form"> <!-- Needs form information -->
-                <button class="Login_Btn" tabindex="3">Log in</button>
-                <input class="TopBar_Password" name="login-password" placeholder="Password" type="password" tabindex="2" autocomplete="off">
-                <input class="TopBar_Username" name="login-email" placeholder="E-mail"  tabindex="1" autocomplete="off">
-            </form>
-        </div>
+<?php
+    include("include/header.php");
+?>
         
         <video playsinline autoplay muted loop id="bgvid">
             <source src="Video/WoW Places - Mists of Pandaria sightseeing.mp4" type="video/mp4">
@@ -65,10 +48,10 @@ and open the template in the editor.
             <p id="bla"></p>
         </div>
         
-        <div class="footer">
-            <div class="footer_logo">AvatarConnect 2017</div>
-        </div>
-        
+        <?php
+            include("include/footer.php");
+        ?>
+
         <!-- Email validation -->
         <script type='text/javascript'>
         $(document).ready(function () {
