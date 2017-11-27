dba69cec17124ee72a210f3bdcd4585accceac10
ibios <jacdam1@hotmail.com>
2017-11-27 17:38:30 +0100

    back end validation
    - added backend validation

--- project/api/sign-up.php
+++ project/api/sign-up.php
@@ -11,6 +11,7 @@ $email = $_POST['email'];
 //Yeah I think we should, mark it down as validation (Jacobs task)
 $password = $_POST['password1'];
 $password2 = $_POST['password2'];
+
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ //
 $fname = $_POST['fname'];
 $lname = $_POST['lname'];
@@ -18,6 +19,45 @@ $avatarname = $_POST['avatarname'];
 
 $mobile = $_POST['phone'];
 $files = $_FILES;
+
+//Set the salt
+$salt = mcrypt_create_iv(16,MCRYPT_DEV_URANDOM);
+$salt = base64_encode($salt);
+
+//Hash it
+$hash = hash("sha256",$password.$salt);
+
+// Checks for input fields being empty
+if(empty($email) || empty($avatarname) || empty($fname) || empty($lname) || empty($mobile) || empty($password) || empty($password2)){
+    echo 'Make sure to fill out every input! <br>';
+}
+else{
+  
+    //Checks if email is correct format
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        echo 'Invalid email format';
+    }
+    else{
+        
+       //Checks if names are valid
+        if(!preg_match('/^[a-zA-Z ]*$/', $fname) || !preg_match('/^[a-zA-Z ]*$/', $lname)){
+           echo'Invalid name format!';
+        } 
+        else{
+            // Checks phone number format
+            if(!preg_match('/^(([1-9]\d{0,2}[ ])|([0]\d{1,3}[-]))((\d{2}([ ]\d{2}){2})|(\d{3}([ ]\d{3})*([ ]\d{2})+))$/i', $mobile)){
+                echo'Invalid phone number';
+            }
+            else{
+                // Checks password format
+                if(!preg_match('/^(?=.*[A-Z])(?=.*[a-z])(?=.*[=?!@&$#^\-])[A-Za-z0-9=?!@&$#^\-]{8,}$/', $password)){
+                    echo'Invalid password';
+                }
+                else{
+                    
+                //Checks if password fields are the same
+                if($password == $password2){
+    
 //Upload the users profile image
 // - returns the images new name
 //If the user is not uploading a new profile image
@@ -26,12 +66,6 @@ if(!isset($_FILES["profileimage"]["name"])){
 }else{
 	$profileimg = uploadProfileImage($files);
 }
-//Set the salt
-$salt = mcrypt_create_iv(16,MCRYPT_DEV_URANDOM);
-$salt = base64_encode($salt);
-
-//Hash it
-$hash = hash("sha256",$password.$salt);
 
 //Remember that the password is the hash.
 $stmt = $conn->prepare("CALL CreateSocialUser(:email, :pass, :salt, :fname, :lname, :avatarname, :profileimg, :mobile)");
@@ -45,4 +79,25 @@ $stmt->bindValue(":profileimg", $profileimg);
 $stmt->bindValue(":mobile", $mobile);
 $stmt->execute();
 echo '{"status":"success"}';
+    
+}
+    else
+    {
+        echo 'Both password field should be the same! <br>';
+    }
+                }
+                
+            }
+
+        }
+
+        
+}      
+         
+}
+
+
+    
+
+
 ?>
