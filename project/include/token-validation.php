afe5d44ea36f7e14252fab416776f7be0dd9093e
Unknown <morten_p_oe@hotmail.com>
2017-12-12 19:40:51 +0100

    Added comments

--- project/include/token-validation.php
+++ project/include/token-validation.php
@@ -1,9 +1,11 @@
 <?php
 
-
+//Get the token from the given form
 $token = $_POST['token'];
+//Sanitize the input
 $token = htmlspecialchars($token, ENT_QUOTES, 'UTF-8');
 
+//Check if the token matches the sessions token
 if($token == $_SESSION['token'])
 {
 	//We good
--- project/include/xsrfprot.php
+++ project/include/xsrfprot.php
@@ -2,9 +2,13 @@
 
 
 //session_start();
+
+//Get the page we SHOULD arrive from
 $myDomain = $_SESSION["org_referer"];
+//Find out where we actually CAME from
 $requestsSource = $_SERVER['HTTP_REFERER'];
 
+//If they do not match, they are coming from another domain, which we should block immediately
 if($requestsSource != $myDomain)
 {
     echo "die scrub";
